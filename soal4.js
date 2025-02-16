34ee0ce5e84520444854cf3460bee77911c0528c
muhammadsandi07 <muhammadsandimaulana07@gmail.com>
2025-02-16 23:07:56 +0700

    Update soal4.js

--- soal4.js
+++ soal4.js
@@ -11,7 +11,7 @@ const printSquare = (number) =>{
     let word = ""
     
     for(let i =1; i<= number; i++){
-        word += i + " "
+        word += i
     }
     for(let i =1; i<= number; i++){
         console.log(word)
